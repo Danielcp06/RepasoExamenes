6f815d83c89ace71a48b8ff06dade33b4ae4f0d4
chamorro <danielchamorropinto1@gmail.com>
2026-04-29 10:12:22 +0200

    repaso

---
+++ src/main/java/Ejercicio9.java
@@ -0,0 +1,39 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+public class Ejercicio9 {
+
+    static void main() {
+
+        Path fichero = Path.of("src/main/Matriculas.txt");
+        Path salida = Path.of("src/main/SalidaMatricula.json");
+        Pattern patron = Pattern.compile("(?<numeros>[0-9]{4})-(?<letras>[A-Z && [^AEIOU]]{3})");
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        try (Stream<String> lineas = Files.lines(fichero)){
+            List<MatriculaDTO> matriculasValidas = lineas.map(l -> patron.matcher(l))
+                    .filter(m -> m.find())
+                    .map(m ->{
+                        String numeros = m.group("numeros");
+                        String letras = m.group("letras");
+                        return new MatriculaDTO(numeros,letras);
+                   })
+                    .toList();
+            String json = gson.toJson(matriculasValidas);
+            Files.writeString(salida,json, StandardOpenOption.CREATE);
+
+        } catch (IOException e) {
+            System.out.println("error");
+        }
+    }
+
+}
